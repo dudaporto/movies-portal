9f52717db5660db36dd7afc626cea14f358d6f23
Duda Porto <dudabatistaporto@gmail.com>
2021-06-18 20:48:42 -0300

    Implementando pesquisa

--- MoviesPortal/script.js
+++ MoviesPortal/script.js
@@ -1,10 +1,61 @@
-
 const apiDomain = "api.themoviedb.org";
 const apiKey = "78dfcd8bdc571ab3d4d798876dc32051";
 var quantity = 4;
 
+function setKeywordSearch() {
+    keyword = $("#input-pesquisar").val();
+    localStorage.setItem("keyword-search", keyword);
+    window.location.href = "search.html";
+}
+
+function getSearchResults() {
+  keyword = localStorage.getItem("keyword-search");
+
+  if(jQuery.isEmptyObject(keyword)) {
+    presentSearchError(keyword);
+    return;
+  }
+
+  $("#section-search search-results").remove();
+  let endpoint = `/3/search/movie`;
+  let params = `api_key=${apiKey}&language=pt-BR&page=1&query=${keyword}&page=1&include_adult=false`;
+
+  let url = `https://${apiDomain}${endpoint}?${params}}`;
+
+  $.getJSON(url, (response) => {
+    const movies = response.results;
+    
+    if(Object.keys(movies).length == 0) {
+      presentSearchError(keyword);
+      return;
+    }
+  
+    $("#texto-busca").html(`Resultados para "${keyword}"`);
+
+    movies.forEach((movie, index) => {
+      const imgUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
+
+      if (movie.poster_path != null) {
+          console.log(movie.id);
+          $("#section-search").append(`
+          
+          <div class="col-12 col-sm-6 col-lg-3 destaque">
+            <a href="https://www.themoviedb.org/movie/${movie.id}" target="_blank">
+              <img src="${imgUrl}" alt="Destaque 01">
+              </a>
+          </div>
+          
+        `);
+      }
+    });
+  });
+}
+
+function presentSearchError(keyword) {
+  $("#texto-busca").html(`Não encontramos resultados para "${keyword}"`);
+}
+
 function getTopRated() {
-    $("#container-top-rated").html("");
     console.log("teste");
     let endpoint = `/3/movie/top_rated`;
     let params = `api_key=${apiKey}&language=pt-BR&page=1`;
@@ -14,17 +65,26 @@ function getTopRated() {
     $.getJSON(url, (response) => {
       const movies = response.results.slice(0, quantity);
 
-      console.log("teste 1");
-  
-      movies.forEach((movie, index) => {
-        const imgUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
-        console.log(imgUrl);
-  
-        $('#container-top-rated').prepend(`
-            <div class="col-12 col-sm-6 col-lg-3 destaque destaque_para_esconder">
-                <img src="${imgUrl}" alt="Destaque 04">
-            </div>
-        `);
-      });
+      if(Object.keys(movies).length > 0) {
+        $(".title_container").removeClass("hidden-div");
+
+        movies.forEach((movie, index) => {
+          const imgUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
+          console.log(imgUrl);
+    
+          $('#em_destaque').append(`
+              <div class="col-12 col-sm-6 col-lg-3 destaque destaque_para_esconder">
+                <a href="https://www.themoviedb.org/movie/${movie.id}" target="_blank">
+                  <img src="${imgUrl}" alt="Destaque 01">
+                </a>
+              </div>
+          `);
+        });
+   
+        $('#em_destaque').append(`
+        <div class="col-12 div_carregar_mais">
+          <button type="button" class="btn btn-outline-warning carregar_mais">Carregar mais filmes</button>
+        </div>`);
+      }
     });
   }
