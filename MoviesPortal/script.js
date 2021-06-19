bf075f3f86fe9cd9cbd2c7ca13922c753d8b1003
Duda Porto <dudabatistaporto@gmail.com>
2021-06-18 23:07:37 -0300

    Implementando lançamentos dinamico

--- MoviesPortal/script.js
+++ MoviesPortal/script.js
@@ -2,6 +2,58 @@ const apiDomain = "api.themoviedb.org";
 const apiKey = "78dfcd8bdc571ab3d4d798876dc32051";
 var quantity = 4;
 
+function loadContent() {
+  getUpcoming()
+  getTopRated()
+}
+
+function getUpcoming() {
+  let endpoint = `/3/movie/upcoming`;
+  let params = `api_key=${apiKey}&language=pt-BR&page=1`;
+
+  let url = `https://${apiDomain}${endpoint}?${params}}`;
+
+  $.getJSON(url, (response) => {
+    const movies = response.results.slice(0, 3);
+
+    if(Object.keys(movies).length > 0) {
+      movies.forEach((movie, index) => {
+        const imgUrl = `https://image.tmdb.org/t/p/original${movie.backdrop_path}`;
+        const movieLink = `https://www.themoviedb.org/movie/${movie.id}`;
+        console.log(movie);
+  
+        $(`#slide-${index}`).append(`      
+            <div class="col-12 col-xl-6 video_coluna">
+                <div class="video">
+                  <a href="${movieLink}" target="_blank">
+                      <img class="" src="${imgUrl}" alt="banner" />
+                  </a>
+                </div>
+            </div>
+
+            <div class="col-12 col-xl-6 descricao_lancamento">
+              <a href=${movieLink} target="_blank">
+                <h2>${movie.title}</h2>
+              </a>
+                <p class="sinopse">
+                    <span class="highlight">Sinopse:</span>  ${movie.overview}
+                </p>
+                <div class="direcao_do_filme">
+                    <text><span class="highlight">Estreia: </span>${movie.release_date}</text>
+                </div>
+
+                <div class="avaliacao_estrelas">
+                    <p><span class="highlight">Avaliação: </span>${movie.vote_average} </p>
+                </div>
+
+            </div>
+        `);
+      });
+      $("#lancamentos").removeClass("hidden-div");
+    }
+  });
+}
+
 function setKeywordSearch() {
     keyword = $("#input-pesquisar").val();
     localStorage.setItem("keyword-search", keyword);
@@ -42,7 +94,7 @@ function getSearchResults() {
           <div class="col-12 col-sm-6 col-lg-3 destaque">
             <a href="https://www.themoviedb.org/movie/${movie.id}" target="_blank">
               <img src="${imgUrl}" alt="Destaque 01">
-              <p>${movie.original_title}</p>
+              <p>${movie.title}</p>
               </a>
           </div>
           
